7755fad1919d8daef44f934ba2e0b1b0ddb1b73b
mikiburgess <11767981+mikiburgess@users.noreply.github.com>
2024-05-09 17:01:26 +0100

    FEAT: Use navbar to sort all products

--- products/views.py
+++ products/views.py
@@ -3,6 +3,8 @@ Product Views for "SERENITEA EMPORIUM"
 - - - - - - - - - - - - - - - - - - - -
 """
 from django.shortcuts import render, get_object_or_404
+from django.db.models.functions import Lower
+
 from .models import Product
 
 
@@ -10,10 +12,28 @@ from .models import Product
 
 def all_products(request):
     """ View to show all products """
-    products = Product.objects.all()
-    
+
+    products = Product.objects.all()  # retrieve all products from database
+    sort = None  # initialise product sorting element to None/Unspecified
+    direction = None  # initialise product sorting order to None/Unspecified
+
+    if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'category':
+                sortkey = 'category__name'
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+
+            products = products.order_by(sortkey)
+            current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
