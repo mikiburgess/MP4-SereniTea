75e32765b8c37711959c3432c6ef9aa9e95f46d4
mikiburgess <11767981+mikiburgess@users.noreply.github.com>
2024-05-09 19:57:17 +0100

    FEAT: Add code to allow user to view products according to category and category group
    Completes all navbar product selection dropdown actions

--- products/views.py
+++ products/views.py
@@ -5,8 +5,9 @@ Product Views for "SERENITEA EMPORIUM"
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.contrib import messages
 from django.db.models import Q  # to generate search queries in Django
+from django.db.models.functions import Lower
 
-from .models import Product
+from .models import Product, Category
 
 
 # Product app views
@@ -46,6 +47,23 @@ def all_products(request):
             products = products.filter(queries)
 
         # Handle user submitting request to view products in specific category
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')  # allow for multiple categories
+            products = products.filter(category__name__in=categories)
+            # get category objects to enable access to category fields
+            categories = Category.objects.filter(name__in=categories)
+
+        # Handle user submitting request to view all products in a specific category group
+        if 'group' in request.GET:
+            group = request.GET['group']
+            # obtain all Category objects where Category.group = requested group
+            categories = Category.objects.filter(group__iexact=group)
+            # iterate through the group categories to extract list of category names
+            names = []
+            for category in categories:
+                names.append(category.name)
+            # filter products based on category name
+            products = products.filter(category__name__in=names)
 
     current_sorting = f'{sort}_{direction}'
 
