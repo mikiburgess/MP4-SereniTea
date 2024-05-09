74ec62016d136701b2eb91e900d86b860d9dabeb
mikiburgess <11767981+mikiburgess@users.noreply.github.com>
2024-05-09 14:48:44 +0100

    ADD: Add pages to view all products and individual product details

--- products/views.py
+++ products/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+"""
+Product Views for "SERENITEA EMPORIUM"
+- - - - - - - - - - - - - - - - - - - -
+"""
+from django.shortcuts import render, get_object_or_404
+from .models import Product
 
-# Create your views here.
+
+# Product app views
+
+def all_products(request):
+    """ View to show all products """
+    products = Product.objects.all()
+    
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """ View to show details of individual products """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
