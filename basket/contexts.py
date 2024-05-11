d83ef48f8c43a9751b7cd6580a8cb20deb1a84d7
mikiburgess <11767981+mikiburgess@users.noreply.github.com>
2024-05-11 11:33:15 +0100

    FIX: Change Floats to Decimals for accuracy & calculations

--- basket/contexts.py
+++ basket/contexts.py
@@ -3,16 +3,36 @@ MILESTONE PROJECT 4 by MIKHAILA BURGESS
 Context Processor for the Basket App
 - - - - - - - - - - - - - - - - - - - -
 """
+from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 
 def basket_contents(request):
+    """ Context processor for basket contents """
+
     # Initialise shopping basket and initial costs
     basket_items = []
     items_count = 0
     items_total = 0
-    standard_delivery = settings.STANDARD_DELIVERY_CHARGE
-    free_delivery_threshold = settings.FREE_DELIVERY_THRESHOLD
+    grand_total = 0.0
+    standard_delivery = Decimal(settings.STANDARD_DELIVERY_CHARGE)
+    free_delivery_threshold = Decimal(settings.FREE_DELIVERY_THRESHOLD)
+
+    # Get current basket contents from session variable
+    basket = request.session.get('basket', {})
+
+    # Update contents of basket
+    for item_id, quantity in basket.items():
+        product = get_object_or_404(Product, pk=item_id)
+        items_total += quantity * product.price
+        items_count += quantity
+        basket_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     # Calculate current costs and delivery charge
     if 0 < items_total < free_delivery_threshold:
@@ -22,7 +42,7 @@ def basket_contents(request):
         delivery = 0
         amount_to_spend = 0
 
-    grand_total = delivery + items_total
+    grand_total = items_total + delivery
 
     # Shopping Basket context data
     context = {
