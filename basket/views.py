db1c23cd666bf1c8b6057b2b23b1c039f0e66351
mikiburgess <11767981+mikiburgess@users.noreply.github.com>
2024-05-11 10:54:08 +0100

    FEAT: Add facility to add items to shopping basket

--- basket/views.py
+++ basket/views.py
@@ -4,9 +4,31 @@ Basket app VIEWS for "SERENITEA EMPORIUM"
 - - - - - - - - - - - - - - - - - - - -
 """
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def view_basket(request):
     """ A view to return the shopping basket page """
     return render(request, 'basket/basket.html')
+
+
+def add_to_basket(request, item_id):
+    """ Add specified quantity of selected item to the basket """
+    
+    # obtain submitted variable values
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+
+    basket = request.session.get('basket', {})
+
+    # add basket/content to session basket
+    if item_id in list(basket.keys()):
+        # item already in basket - increment quantity
+        basket[item_id] += quantity
+    else:
+        # item not in basket - add quantity to basket
+        basket[item_id] = quantity
+
+    request.session['basket'] = basket
+    print(request.session['basket'])
+    return redirect(redirect_url)
