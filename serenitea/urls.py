d484c9769c5f942d0317b4aad247a227e8a408b8
mikiburgess <11767981+mikiburgess@users.noreply.github.com>
2024-05-12 15:50:11 +0100

    DOCS: Improve comments in urls.py
    Remove unnecessary guidance comments included by default in django

--- serenitea/urls.py
+++ serenitea/urls.py
@@ -1,17 +1,7 @@
-"""serenitea URL Configuration
+""" SereniTea URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    Contains the `urlpatterns` list routes 
+    URLs to all SereniTea app views. 
 """
 from django.contrib import admin
 from django.urls import path, include
@@ -24,4 +14,5 @@ urlpatterns = [
     path('', include('home.urls')),
     path('products/', include('products.urls')),
     path('basket/', include('basket.urls')),
+    path('checkout/', include('checkout.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
