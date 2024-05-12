c9b341deea3de9496e826fe448c8357ab43ebd15
mikiburgess <11767981+mikiburgess@users.noreply.github.com>
2024-05-12 09:48:33 +0100

    ADD: Add Checkout app to project

---
+++ checkout/models.py
@@ -0,0 +1,77 @@
+"""
+Checkout Models for "SERENITEA EMPORIUM"
+- - - - - - - - - - - - - - - - - - - -
+"""
+import uuid  # for generating order numbers
+
+from django.db import models
+from django.db.models import Sum
+from django.conf import settings
+from django_countries.fields import CountryField
+
+from products.models import Product
+
+
+# Model for order placed by shop customers
+class Order(models.Model):
+    # Order Details
+    order_number = models.CharField(max_length=32, null=False, editable=False)
+    delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
+    order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    date = models.DateTimeField(auto_now_add=True)
+    # Customer Details
+    title = models.CharField(max_length=10, null=True, blank=True)
+    first_name = models.CharField(max_length=50, null=False, blank=False)
+    surname = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    phone_number = models.CharField(max_length=20, null=False, blank=False)
+    # Customer Address
+    street_address1 = models.CharField(max_length=80, null=False, blank=False)
+    street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    town_or_city = models.CharField(max_length=40, null=False, blank=False)
+    county = models.CharField(max_length=80, null=True, blank=True)
+    postcode = models.CharField(max_length=20, null=True, blank=True)
+    country = CountryField(blank_label='Country *', max_length=40, null=False, blank=False)
+
+    def _generate_order_number(self):
+        """ Generate a random, unique order number using UUID """
+        return uuid.uuid4().hex.upper()
+
+    def __str__(self):
+        """ Return order_number attribute when name of order model called """
+        return self.order_number
+
+    def update_total(self):
+        """ Update order grand total each time a line item is added, accounting for delivery costs. """
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
+        if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
+            self.grand_total = self.order_total + settings.STANDARD_DELIVERY_CHARGE
+        else:
+            self.grand_total = self.order_total
+        self.save()
+
+    def save(self, *args, **kwargs):
+        """ Override the original save method to generate new order number 
+            if it hasn't been generated already .
+        """
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+
+
+# Model for order lines in each orders
+class OrderLineItem(models.Model):
+    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
+    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
+    quantity = models.IntegerField(null=False, blank=False, default=0)
+    lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
+
+    def save(self, *args, **kwargs):
+        """ Override the original save method to set the lineitem total and update the order total """
+        self.lineitem_total = self.product.price * self.quantity
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        """ Return SKU and order number when name of order line model called """
+        return f'SKU {self.product.sku} on order {self.order.order_number}'
