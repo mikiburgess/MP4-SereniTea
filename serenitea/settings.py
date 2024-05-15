faf483ced541fbaa337855e549c0c09a10fc1b65
mikiburgess <11767981+mikiburgess@users.noreply.github.com>
2024-05-15 20:32:04 +0100

    FIX: Add correct Heroku URL to settings.py

--- serenitea/settings.py
+++ serenitea/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['mp4-serenitea-emporium.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['mp4-serenitea-emporium-5454dc22e46f.herokuapp.com', 'localhost']
 
 
 # Application definition
