134c68f8821e680e9efda56a95190070780ee808
mikiburgess <11767981+mikiburgess@users.noreply.github.com>
2024-05-16 17:20:57 +0100

    FIX: Modify whitenoise in settings.py

--- serenitea/settings.py
+++ serenitea/settings.py
@@ -231,8 +231,8 @@ STRIPE_SECRET_KEY = os.environ.get("STRIPE_SECRET_KEY")
 STRIPE_WH_SECRET = os.environ.get("STRIPE_WH_SECRET")
 DEFAULT_FROM_EMAIL = 'confirmation-noreply@serenitea.com'
 
-
-STATIC_ROOT = BASE_DIR / "staticfiles"
+# For Whitenoise
+# STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
