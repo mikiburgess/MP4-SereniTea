8ad6bc9de9c68c630d97e660acc087f653ecf544
Dr Nic Caine <ncaine@gmail.com>
2024-05-17 15:56:32 +0100

    Update settings.py
    use aws fales

--- serenitea/settings.py
+++ serenitea/settings.py
@@ -184,7 +184,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 
-USE_S3 = os.getenv('USE_S3') == 'TRUE'
+USE_S3 = os.getenv('USE_S3') == 'FALSE'
 
 if USE_S3:
    # Cache control
