2d37b583ad9fd07b92f9a005748dbdd937af3fef
Dr Nic Caine <ncaine@gmail.com>
2024-05-17 13:58:43 +0100

    Update settings.py
    reverted changes

--- serenitea/settings.py
+++ serenitea/settings.py
@@ -183,10 +183,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-DISABLE_COLLECTSTATIC = 1
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
