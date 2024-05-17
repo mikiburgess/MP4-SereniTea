4ceec062f7c3554f30160f38574cc4f83825e01f
Dr Nic Caine <ncaine@gmail.com>
2024-05-17 13:55:53 +0100

    Update settings.py
    added DISABLE_COLLECTSTATIC = 1

--- serenitea/settings.py
+++ serenitea/settings.py
@@ -183,6 +183,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+DISABLE_COLLECTSTATIC = 1
 
 STATIC_URL = '/static/'
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
