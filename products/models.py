dcfe0b0d17282cc0d118375c8a8b4eed8cdee786
mikiburgess <11767981+mikiburgess@users.noreply.github.com>
2024-05-23 16:56:20 +0100

    UPDATE: Add default values to mandatory fields in product & category models

--- products/models.py
+++ products/models.py
@@ -10,7 +10,8 @@ class Category(models.Model):
     class Meta:
         verbose_name_plural = 'Categories'
 
-    friendly_name = models.CharField(max_length=200, null=False, blank=True)
+    friendly_name = models.CharField(max_length=200, null=False,
+                                     default="Product Category")
     name = models.CharField(max_length=200)
     group = models.CharField(max_length=100, null=True, blank=True)
 
@@ -26,10 +27,11 @@ class Product(models.Model):
     class Meta:
         verbose_name_plural = 'Products'
 
-    category = models.ForeignKey('Category', null=True, blank=True, 
+    category = models.ForeignKey('Category', null=True, blank=True,
                                  on_delete=models.SET_NULL)
     sku = models.CharField(max_length=10, null=True, blank=True)
-    friendly_name = models.CharField(max_length=200, null=False, blank=True)
+    friendly_name = models.CharField(max_length=200, null=False,
+                                     default="Store Product")
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
