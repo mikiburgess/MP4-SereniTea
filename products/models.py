a01330736f0f0cc701e178dfe7e783112b9ce7b8
mikiburgess <11767981+mikiburgess@users.noreply.github.com>
2024-05-23 12:53:30 +0100

    UPDATE: Modify Product model to reorder elements and update null falues

--- products/models.py
+++ products/models.py
@@ -10,8 +10,8 @@ class Category(models.Model):
     class Meta:
         verbose_name_plural = 'Categories'
 
+    friendly_name = models.CharField(max_length=200, null=False, blank=True)
     name = models.CharField(max_length=200)
-    friendly_name = models.CharField(max_length=200, null=True, blank=True)
     group = models.CharField(max_length=100, null=True, blank=True)
 
     def __str__(self):
@@ -22,11 +22,15 @@ class Category(models.Model):
 
 
 class Product(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Products'
+
     category = models.ForeignKey('Category', null=True, blank=True, 
                                  on_delete=models.SET_NULL)
     sku = models.CharField(max_length=10, null=True, blank=True)
+    friendly_name = models.CharField(max_length=200, null=False, blank=True)
     name = models.CharField(max_length=200)
-    friendly_name = models.CharField(max_length=200, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
     alt_text = models.TextField(null=True, blank=True)
@@ -35,7 +39,7 @@ class Product(models.Model):
     strength = models.IntegerField(null=True, blank=True)
     brew_time = models.CharField(max_length=10, null=True, blank=True)
     water_temp = models.IntegerField(null=True, blank=True)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=6, null=False, decimal_places=2)
     stock_level = models.IntegerField(null=True, blank=True)
     rating = models.DecimalField(max_digits=4, decimal_places=2, null=True, 
                                  blank=True)
@@ -45,3 +49,6 @@ class Product(models.Model):
 
     def get_friendly_name(self):
         return self.friendly_name
+
+    def get_summary(self):
+        return (f'{self.friendly_name} [sku: {self.sku[0:6]}] @ £{self.price}')
