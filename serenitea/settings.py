c944d0b688fb26f303eb8fae41172d511bea29a2
mikiburgess <11767981+mikiburgess@users.noreply.github.com>
2024-06-03 13:43:15 +0100

    UPDATE: Update debug settings

--- serenitea/settings.py
+++ serenitea/settings.py
@@ -25,14 +25,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG")
+# DEBUG = os.environ.get("DEBUG")
+DEBUG = 'DEVELOPMENT' in os.environ
+
 
 ALLOWED_HOSTS = [
     'mp4-serenitea-emporium-5454dc22e46f.herokuapp.com',
     '127.0.0.1',
     'localhost',
 ]
-
 
 # Application definition
 
